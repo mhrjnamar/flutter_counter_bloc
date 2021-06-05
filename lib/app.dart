b32414874faf5de15ddb1fb4ae122a9e49a07b49
Amar <amar@bottle.com.np>
2021-06-05 15:53:43 +0545

    ios build fix

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,6 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:flutter_counter_bloc/counter/counter.dart';
 
-class CounterApp extends MaterialApp{
-  const CounterApp({Key? key}): super(key:key, home: const CounterApp());
+class CounterApp extends MaterialApp {
+  const CounterApp({Key? key}) : super(key: key, home: const CounterPage());
 }
